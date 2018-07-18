320f9f89e42e342335b7ccd60cce4e61a167a5f2
Jean Catanho <jensmcatanho@gmail.com>
2018-07-18 05:45:06 -0300

    week1: fix indentation

--- week1/1_mean_of_a_1d_array/program.py
+++ week1/1_mean_of_a_1d_array/program.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 def calc_stats(file):
-  data = np.loadtxt(file, delimiter=',')
-  mean = np.mean(data)
-  median = np.median(data)
-  return np.round(mean, decimals=1), np.round(median, decimals=1)
+    data = np.loadtxt(file, delimiter=',')
+    mean = np.mean(data)
+    median = np.median(data)
+    return np.round(mean, decimals=1), np.round(median, decimals=1)
 
 if __name__ == '__main__':
-  # Test Case 1
-  print(calc_stats('data.csv'))
+    # Test Case 1
+    print(calc_stats('data.csv'))
 
-  # Test Case 2
-  print(calc_stats('data2.csv'))
+    # Test Case 2
+    print(calc_stats('data2.csv'))
 
-  # Test Case 3
-  print(calc_stats('data3.csv'))
+    # Test Case 3
+    print(calc_stats('data3.csv'))
