be5b3065daff3f4d30522503ef4885bcc52e137c
Jean Catanho <jensmcatanho@gmail.com>
2018-07-18 01:14:10 -0300

    week1: add "Mean of a set of signals" assignment

---
+++ week1/2_mean_of_a_set_of_signals/program.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def mean_datasets(files):
+  datasets = []
+
+  for file in files:
+    datasets.append(np.loadtxt(file, delimiter=','))
+
+  result = np.mean(datasets, axis=0)
+  return np.round(result, decimals=1)
+
+if __name__ == '__main__':
+  # Test Case 1
+  print(mean_datasets(['data1.csv', 'data2.csv', 'data3.csv']))
+
+  # Test Case 2
+  print(mean_datasets(['data4.csv', 'data5.csv', 'data6.csv']))
